30e040405be75890b0e1a645d72486abe091f7cd
pavelsuk <suk.pavel@gmail.com>
2014-08-30 20:09:43 +0200

    Hint how to remove it from watched files

--- web/js/private/sparkdevice.js
+++ web/js/private/sparkdevice.js
@@ -1,6 +1,12 @@
 
 // Just an example - you need to change your device id and access token
-// this file should never be commited back
+
+// This file should never be commited back - run
+// git update-index --assume-unchanged web/js/private/sparkdevice.js
+// and if you want to change it back: 
+// git update-index --no-assume-unchanged web/js/private/sparkdevice.js
+
+
 
 var url2SparkCore = "https://api.spark.io/v1/devices/<your_device_id>/"
 var access_token_param = "access_token=<your_access_token>"
