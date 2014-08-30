c18fbdcea497684ad03434b93280f126264813c9
pavelsuk <suk.pavel@gmail.com>
2014-08-30 19:08:01 +0200

    Initial raw version of web application

---
+++ web/js/private/sparkdevice.js
@@ -0,0 +1,5 @@
+
+// Just an example - you need to change your device id and access token
+
+var url2SparkCore = "https://api.spark.io/v1/devices/<your_device_id>/"
+var access_token_param = "access_token=<your_access_token>"
